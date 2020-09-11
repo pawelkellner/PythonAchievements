4ad10db314f61c335369365507637daac0848b58
Paweł Kellner <karkuwa@Pawel-MacBook.local>
2020-09-11 13:06:22 +0200

    Gedicht

---
+++ Poem.py
@@ -0,0 +1,6 @@
+vers1 = "Mijn naam is Pawel Kellner."
+vers2 = "Ik woon in beverwijk."
+vers3 = "Ik ga naar school met de trein"
+vers4 = "Ik skateboard graag met vrienden"
+
+print(vers1, vers2, vers3, vers4)
