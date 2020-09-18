f13e7f2d2e4a10d6a5675c1a73f78e00709a4c05
Paweł Kellner <karkuwa@Pawel-MacBook.local>
2020-09-18 11:51:22 +0200

    5 vragen

---
+++ Choices.py
@@ -0,0 +1,47 @@
+print("Je bent wakker.")
+print("Wat wil je doen? \n A: Eten \n B: Verder slapen \n C: Douchen")
+
+ant_1 = input()
+
+if ant_1.lower() == "a":
+    print("Je gaat eten")
+elif ant_1.lower() == "b":
+    print("Je gaat verder slapen")
+elif ant_1.lower() == "c":
+    print("Je gaat nu douchen")
+
+print("wat wil je nu gaan doen? \n A: Gamen \n B: Je kamer schoonmaken")
+
+ant_2 = input()
+
+if ant_2.lower() == "a":
+    print("Je gaat nu lekker gamen")
+elif ant_2.lower() == "b":
+    print("je gaat nu schoonmaken")
+
+print("wat wil je nu gaan doen? \n A: Buiten chillen \n B: Drinken kopen")
+
+ant_3 = input()
+
+if ant_3.lower() == "a":
+    print("Je gaat nu buiten chillen")
+elif ant_3.lower() == "b":
+    print("Je gaat nu drinken halen")
+
+print("wat wil je nu gaan doen? \n A: Pizza maken \n B: Osu spelen")
+
+ant_4 = input()
+
+if ant_4.lower() == "a":
+    print("Je gaat nu lekker pizza maken en daarna eten")
+elif ant_4.lower() == "b":
+    print("je gaat nu osu spelen")
+
+print("wat wil je nu gaan doen? \n A: Slapen \n B: Naar de bioscoop")
+
+ant_5 = input()
+
+if ant_5.lower() == "a":
+    print("Je gaat nu lekker slapen")
+elif ant_5.lower() == "b":
+    print("je gaat nu naar de bioscoop")
